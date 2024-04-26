50cb7b620be975d9232dc70710fdd1e80190e1a0
JesusOfLagos <jesuswrites20043@gmail.com>
2024-04-26 18:01:49 +0100

    conflict fixed

---
+++ Node-Engine/start.ts
@@ -0,0 +1,32 @@
+import express, { Express, NextFunction, Request, Response } from 'express';
+import cors from 'cors'
+import { connectToDB } from './database';
+import config from './Config/config';
+import { authRouter } from './Resources/Auth/auth.router';
+import { Cors } from './Config/cors';
+import { transactionRouter } from './Resources/Transactions/transaction.router';
+
+const CavistaNode: Express = express();
+const rettyCors = new Cors;
+
+CavistaNode.use(express.json());
+CavistaNode.use(express.urlencoded({ extended: true }));
+CavistaNode.use(cors(rettyCors.corsOptions))
+
+CavistaNode.use('/api/v1/auth', authRouter)
+CavistaNode.use('/api/v1/app/transactions', transactionRouter)
+
+CavistaNode.use((req: Request, res: Response, next: NextFunction) => {
+    return res.status(404).json({ message: 'This Request does not sit with Retty API' });
+})
+
+CavistaNode.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Internal Server Error' });
+});
+connectToDB();
+CavistaNode.listen(config.app.port, () => {
+    console.log(`Server started on port ${config.app.port}`);
+})
+
+
